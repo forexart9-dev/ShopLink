0d36d4c575c565ad51c9cff442a078980e11c85e
TonNom <tonemail@gmail.com>
2026-06-20 00:53:21 +0200

    Add initial HTML structure and center text in CSS

---
+++ script.js
@@ -0,0 +1,4 @@
+// Interaction simple avec le bouton
+document.getElementById("btn").addEventListener("click", function() {
+  document.getElementById("message").innerText = "Bravo Giscard 🎉, tu as cliqué sur le bouton !";
+});
